0d7f85765fea629cec7f508f206379b3bb38755f
sameer1653 <120358651+sameer1653@users.noreply.github.com>
2023-07-24 14:51:07 +0530

    added

---
+++ script.js
@@ -0,0 +1,54 @@
+// Toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active'); // Add this line to toggle the 'active' class on the navigation menu
+};
+
+// Scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        // active section for animate on scrool
+            sec.classList.add('show-animate');
+        }
+        else{
+            sec.classList.remove('show-animate');
+        }
+        
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when clicked
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+    
+
+    // animate footer on scrool
+
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', window.innerHeight + window.scrollY >= document.scrollingElement.scrollHeight);
+    
+
+
+};
+
+
+
